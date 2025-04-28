56e43c655c67e87d56808a1e518315c6c3301d01
Sumit-udhani <sumit.udhani@nexuslinkservices.in>
2025-04-28 16:45:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+import { Route, Routes,BrowserRouter,Navigate } from 'react-router-dom'
+import Signup from './pages/Signup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter> 
+      <Routes>
+      <Route path='/' element={<Navigate to="/signup" />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import { TextField, Button, Box, Typography } from '@mui/material';
+export default function Signup() {
+   const [form,setForm] =  useState({name:'',email:'',password:''})
+   const [error,setError] = useState('')
+   const navigate = useNavigate()
+   const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try {
+        
+        const response = await axios.post('http://localhost:8085/auth/signup',form)
+        navigate('/verify-email', { state: { email: form.email } });
+    } catch (error) {
+        setError(err.response?.data?.message || 'An error occurred');
+    }
+  }
+  return (
+    <Box sx={{ maxWidth: 400, margin: 'auto', padding: 2 }}>
+      <Typography variant="h4" gutterBottom>Signup</Typography>
+      {error && <Typography color="error">{error}</Typography>}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+          required
+        />
+        <TextField
+          label="Email"
+          name="email"
+          type="email"
+          value={form.email}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+          required
+        />
+        <TextField
+          label="Password"
+          name="password"
+          type="password"
+          value={form.password}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+          required
+        />
+        <Button type="submit" variant="contained" fullWidth sx={{ marginTop: 2 }}>
+          Sign Up
+        </Button>
+      </form>
+    </Box>
+  );
+}
